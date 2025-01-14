51b0ca3c89a3668470ff5d6fcee50ada8c80d01a
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2025-01-14 22:21:39 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
-// write code for Welcome component here
+import React from 'react';
+
+const Welcome = ({ name }) => {
+  if (!name) name = 'Guest'; // Default to 'Guest' if no name is provided
+
+  return (
+    <div>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to School.</h2>
+    </div>
+  );
+};
+
+export default Welcome;
